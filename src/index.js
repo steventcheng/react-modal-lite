aa22fac27ebf6a9b48eac49e4b1c555efaba3c3b
scheng <scheng@wm.com>
2018-07-02 17:09:05 -0500

    Update modal.

--- src/index.js
+++ src/index.js
@@ -8,30 +8,43 @@ export default class Modal extends Component {
   static propTypes = {
     // Contents to display in modal.
     children: PropTypes.node.isRequired,
-    // Hide modal when clicking outside of the modal.
-    enableClickOut: PropTypes.bool,
+    // Set true to show the modal on mount.
+    defaultVisible: PropTypes.bool,
     // Disable hiding the modal on escape key press.
     disableEscape: PropTypes.bool,
+    // Disable hiding the modal when clicking outside of the modal.
+    disableClickOut: PropTypes.bool,
+    // Whether or not the modal is visible.
+    isVisible: PropTypes.bool,
     // Function called when the modal is hidden.
     onClose: PropTypes.func,
     // Show close button that hides the modal when clicked.
-    showClosebutton: PropTypes.bool,
-    // Whether or not the modal is visible.
-    isVisible: PropTypes.bool,
+    showCloseButton: PropTypes.bool,
+    // CSS class name to be applied to the outer-most container.
+    className: PropTypes.string,
+    // CSS class name to be applied to the modal foreground.
+    foregroundClassName: PropTypes.string,
+    // Styles for the modal applied to the outer-most container.
+    style: PropTypes.object,
   }
 
   static defaultProps = {
-    enableClickOut: true,
+    defaultVisible: false,
     disableEscape: false,
+    className: ``,
+    disableClickOut: false,
+    foregroundClassName: ``,
+    isVisible: true,
     onClose: () => {},
-    showClosebutton: false,
-    isVisible: false,
+    showCloseButton: false,
+    style: {},
   }
 
   constructor(props) {
     super(props)
+
     this.state = {
-      isVisible: props.isVisible,
+      isVisible: props.defaultVisible,
     }
   }
 
@@ -39,18 +52,12 @@ export default class Modal extends Component {
     window.addEventListener(`keydown`, this.listenKeyboard, true)
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      isVisible: nextProps.isVisible,
-    })
-  }
-
   componentWillUnmount() {
     window.removeEventListener(`keydown`, this.listenKeyboard, true)
   }
 
   listenKeyboard = (e) => {
-    if (!this.props.disableEscape && e.keyCode === 27) {
+    if (this.props.disableEscape === false && e.keyCode === 27) {
       this.hide()
     }
   }
@@ -62,40 +69,33 @@ export default class Modal extends Component {
     this.props.onClose()
   }
 
-  show() {
-    this.setState({
-      isVisible: true,
-    })
-  }
-
   render() {
-    const {
-      enableClickOut,
-      showClosebutton,
-    } = this.props
-
     return (
-      <div className='Modal'>
-        { this.state.isVisible ? (
+      <div
+        className={`Modal${this.props.className !== `` ? ` ${this.props.className}` : ``}`}
+        styles={this.props.style}
+      >
+        { this.state.isVisible || this.props.isVisible ? (
           <div
             className='modal-container'
             onClick={() => {
-              if (enableClickOut) {
+              if (this.props.disableClickOut === false) {
                 this.hide()
               }
             }}
           >
             <div className='flexbox-center-container'>
               <div
-                className='modal-foreground'
+                className={`modal-foreground${this.props.foregroundClassName !== `` ? ` ${this.props.foregroundClassName}` : ``}`}
                 onClick={(e) => { e.stopPropagation() }}
               >
-                { showClosebutton &&
-                  <div className='close-btn-container'>
+                { this.props.showCloseButton === true
+                  ? <div className='close-btn-container'>
                     <button className='btn-close' onClick={() => { this.hide() }}>
                       <Isvg src={iconCloseSvg} />
                     </button>
                   </div>
+                  : null
                 }
                 {this.props.children}
               </div>
