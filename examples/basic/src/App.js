9087be4767641c52638b863f81f8dc0e8e73b090
Steven Cheng <scjoy8@gmail.com>
2019-10-16 22:01:14 -0400

    Update basic example (#18)
    * Update webpack version
    
    * Update basic example

--- examples/basic/src/App.js
+++ examples/basic/src/App.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-modal-lite'
-import logo from './logo.svg'
-import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Basic modal example</h1>
-        </header>
-        <Modal isVisible>Basic modal example.</Modal>
-      </div>
-    )
-  }
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  return (
+    <div className='App' style={{
+      textAlign: 'center'
+    }}>
+      <header className='App-header'>
+        <h1 className='App-title'>Basic modal example</h1>
+      </header>
+      <main>
+        <button
+          onClick={() => {
+            setIsModalOpen(true)
+          }}
+          >Open modal</button>
+      </main>
+      <Modal
+        isVisible={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(false)
+        }}
+        showCloseButton
+        >Basic modal example.</Modal>
+    </div>
+  )
 }
 
 export default App
